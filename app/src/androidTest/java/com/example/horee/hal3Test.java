cb8f3900a9175fc4744855d0b4f5c557784a3b52
AtanasiusIvannoel <50091059+darkPigeonn@users.noreply.github.com>
2021-05-19 06:43:15 +0700

    Initial commit

---
+++ app/src/androidTest/java/com/example/horee/hal3Test.java
@@ -0,0 +1,7 @@
+package com.example.horee;
+
+import junit.framework.TestCase;
+
+public class hal3Test extends TestCase {
+
+}
---
+++ app/src/main/java/com/example/horee/hal4.java
@@ -0,0 +1,14 @@
+package com.example.horee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hal4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hal4);
+    }
+}
